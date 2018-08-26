488c9940bd4ad0c6c8678a64fa4faec810cb65c3
Augus-top <acpluschkat@hotmail.com>
2018-08-26 10:55:32 -0300

    Adding colors for node types :art:

--- public/javascripts/graphScript.js
+++ public/javascripts/graphScript.js
@@ -1,17 +1,49 @@
+const colors = ['#FFD123', '#0fcbaa', '#6EEB83', '#FF5714', '#1BE7FF', '#C823FF', '#FF0000', '#B5651D'];
+
 const initializeGraph = async () => {
   const graphData = await receiveGraphData();
+  const types = findOutTypes(graphData.nodes);
+  const nodeStyles = prepareNodeStyles(graphData.nodes, types.type);
   const config = {
     // dataSource: graphData,
     dataSource: 'api/graph',
     forceLocked: true,
     linkDistance: () => { return 40; },
-  
-    nodeTypes: {"type":["curso", "periodo", "disciplina"]},
+    nodeTypes: types,
+    nodeStyle: nodeStyles,
     caption: (node) => { 
         return node.caption; 
     }
   };
   alchemy = new Alchemy(config);
+};
+
+const prepareNodeStyles = (nodes, types) => {
+  const style = {"all": 
+  {
+    radius: 10, 
+    color: "#faf", 
+    borderColor: "#fff", 
+    borderWidth: 2,
+  }};
+  types.forEach((t, i) => {
+    style[t] = {
+      color: colors[i % colors.length],
+      selected: {
+        color: "#FFFFFF",
+        borderColor: "#349FE3"
+      },
+      highlighted: {
+        color: "#EEEEFF"
+      }
+    };
+  });
+  return style;
+};
+
+const findOutTypes = (nodes) => {
+  const types = new Set(nodes.map(n => n.type));
+  return ({type: [...types]});
 };
 
 const receiveGraphData = async () => {
